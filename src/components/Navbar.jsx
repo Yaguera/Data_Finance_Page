dce1b8d369d1bc4558799648bb509f2a4e89907b
Yaguera <ya-gotre@hotmail.com>
2024-01-12 11:35:25 -0300

    Navbar mobile and desktop complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () =>{
+    setNav(!nav)
+  }
+
   return (
     <div  className='text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
-      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT</h1>
-      <ul className='flex'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className='hidden md:flex'>
         <li className='p-4'>Home</li>
         <li className='p-4'>Company</li>
         <li className='p-4'>Resources</li>   
         <li className='p-4'>About</li>
         <li className='p-4'>Contact</li>
       </ul>
-        <AiOutlineMenu size={30}/>
+      <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <AiOutlineClose size={20}/>:<AiOutlineMenu size={20}/>}
+      </div>
+      <div className={nav ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed top-0 left-[-100%] ease-in-out duration-1000'}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <ul className='uppercase p-4'>
+          <li className='p-4 border-b border-b-gray-600'>Home</li>
+          <li className='p-4 border-b border-b-gray-600'>Company</li>
+          <li className='p-4 border-b border-b-gray-600'>Resources</li>   
+          <li className='p-4 border-b border-b-gray-600'>About</li>
+          <li className='p-4 border-b border-b-gray-600'>Contact</li>
+        </ul>
+      </div>
     </div>
   )
 }
