f86d601be8a91c72295608562af4aa280bb80307
Yaguera <ya-gotre@hotmail.com>
2024-01-11 22:18:03 -0300

    add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div  className='text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT</h1>
+      <ul className='flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Company</li>
+        <li className='p-4'>Resources</li>   
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
