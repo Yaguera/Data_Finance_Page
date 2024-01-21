d0733e74eec20d87f82f4335acde828c398aa8c4
Yaguera <ya-gotre@hotmail.com>
2024-01-21 14:10:15 -0300

    Update navbar animation and add .gitignore entry
    - Modify the navbar transition to use transform for smoother animation
    - Add a new entry to .gitignore for easycode.ignore
    - Remove unnecessary comments from index.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
       <div onClick={handleNav} className='block md:hidden'>
         {nav ? <AiOutlineClose size={20}/>:<AiOutlineMenu size={20}/>}
       </div>
-      <div className={nav ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed top-0 left-[-100%] ease-in-out duration-1000'}>
+      <div className={nav ? 'fixed md:hidden top-0 w-[60vw]  bg-[#000300] ease-in-out duration-500 transform translate-x-0' : 'fixed md:hidden top-0 w-[60vw] bg-[#000300] ease-in-out duration-500 transform -translate-x-[120%]'}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
         <ul className='uppercase p-4'>
           <li className='p-4 border-b border-b-gray-600'>Home</li>
@@ -31,6 +31,11 @@ const Navbar = () => {
           <li className='p-4 border-b border-b-gray-600'>Contact</li>
         </ul>
       </div>
+
+
+
+
+
     </div>
   )
 }
