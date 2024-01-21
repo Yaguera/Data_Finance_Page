1dbabb2c119450a3e046ac082382c3927013a3be
Yaguera <ya-gotre@hotmail.com>
2024-01-21 16:23:50 -0300

    "Add button to Hero component"

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,13 +26,14 @@ const typed = new Typed(el.current, {
     }, []);
 return (
     <div className='text-white'>
-        <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center ">
             <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
             <h1 className='text-4xl sm:text-6xl md:text-7xl font-bold md:py-6'>Grow with data</h1>
             <div className='flex justify-center items-center'>
                 <p className='text-xl sm:text-4xl md:text-5xl font-bold py-4'>Fast, flexible financing for <span ref={el}></span></p>
             </div>
             <p className='text-xl md:text-2xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC & SASS plataforms.</p>
+            <button className='bg-[#00df9a] rounded-md w-[200px] mx-auto font-medium my-6 px5 py-3 text-black'>Get Started</button>
         </div>
     </div>
 )
